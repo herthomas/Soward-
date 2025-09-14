8b6cdfae874996825a8a14946ce183ed3cc6180d
her.thomas.js <schetry031@gmail.com>
2025-09-14 11:19:18 +0530

    Add files via upload

---
+++ themes/urls.py
@@ -0,0 +1,13 @@
+SUPPORT_SERVER = "https://discord.gg/soward"
+WEBSITE = "https://soward.app/"
+PRIVACY_POLICY = "https://soward.app/privacy"
+TERMS_OF_SERVICE = "https://soward.app/terms"
+VOTE = "https://top.gg/bot/1013771497157972008/vote"
+ANTINUKE = "https://cdn.discordapp.com/attachments/1267073545482010728/1274781772655104053/5c6bc2fc0e2f1c8042d9adca_antinuke.png?ex=66c3808a&is=66c22f0a&hm=0b9d9694826baf4f400a02b802b048cd456876d54577d76d123f3977181c5b48&"
+INVITE = r"https://discord.com/oauth2/authorize?client_id=1013771497157972008&permissions=8&response_type=code&redirect_uri=https%3A%2F%2Fdiscord.gg%2Fsoward&integration_type=0&scope=gdm.join+bot"
+GIVEAWAY = "https://cdn.discordapp.com/attachments/1285086383957868576/1286048893338914827/realistic-3d-gift-box-cutout-free-png.png?ex=66ec7ddd&is=66eb2c5d&hm=a4fa5434641a86a0c5cbbfcd78e8bf2b1ddf503bd6dce28c594ad518f3f832fc&"
+TICKET = "https://cdn.discordapp.com/attachments/1286969360224882688/1286969378146877461/6030249.png?ex=66efd722&is=66ee85a2&hm=d1c77d1c703d997bf2742b39fda05757f64ccd3b7758dd1bc9a0ea469507fd35&"
+
+DEFAULT_MUSIC_BANNER = "https://cdn.discordapp.com/attachments/1286969360224882688/1290696536514363392/pngtree-purple-minimalist-music-note-banner-background-image_233650.jpg?ex=66fd6652&is=66fc14d2&hm=3807f509b1d9090d59e3eb9ea7a974035f5159a7b79c5847b10fc78273f0b3d4&"
+
+TRANSCRIPT_BASE_URL = "https://beta.soward.app"
